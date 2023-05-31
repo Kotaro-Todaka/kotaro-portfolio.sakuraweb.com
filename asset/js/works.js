b87f4fc384a956c4b66406353aaa351da9510c19
Kotaro Todaka <todakan.kotaro.0909@gmail.com>
2023-06-01 00:27:02 +0900

    それぞれの変数名が間違った命名規則でつけられていたため変更しました

--- asset/js/works.js
+++ asset/js/works.js
@@ -1,22 +1,22 @@
 // スクロールアニメーション
 // 作品詳細ページで、メインビジュアルを過ぎたら表示される、外部リンクボタン
-const SITE_LINK_TRIGGER = document.querySelector(".js-linkButtonTrigger");
-const SITE_LINK_BUTTON = document.querySelector(".js-works__button");
+const siteLinkTrigger = document.querySelector(".js-linkButtonTrigger");
+const siteLinkButton = document.querySelector(".js-works__button");
 
-// SITE_LINK_OBSERVERオプション
-const OPTIONS_02 = {
+// siteLinkObserverオプション
+const option02 = {
   rootMargin: '-50% 0px'
 };
 
-const SITE_LINK_OBSERVER = new IntersectionObserver((entries) => {
+const siteLinkObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      SITE_LINK_BUTTON.classList.add(SHOW_CLASS);
+      siteLinkButton.classList.add(showClass);
     } else {
-      SITE_LINK_BUTTON.classList.remove(SHOW_CLASS);
+      siteLinkButton.classList.remove(showClass);
     }
   });
 });
 
 // 監視対象02：.js-siteLinkButtonTriggerが入っている要素
-SITE_LINK_OBSERVER.observe(SITE_LINK_TRIGGER, OPTIONS_02);
+siteLinkObserver.observe(siteLinkTrigger, option02);
